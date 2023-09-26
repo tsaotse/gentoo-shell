e3abe3666dd1e8facce8448039dec8a1c2eaaabf
tsaotse <78033148+tsaotse@users.noreply.github.com>
2023-09-26 11:35:38 +0800

    Add files via upload

---
+++ portage.sqfs.sh
@@ -0,0 +1,13 @@
+umount -l /usr/portage
+sleep 1
+mv -v /home/gentoo-current.xz.sqfs /home/old.gentoo-current.xz.sqfs
+sleep 1
+wget -c -O /home/gentoo-current.xz.sqfs https://mirrors.tuna.tsinghua.edu.cn/gentoo/snapshots/squashfs/gentoo-current.xz.sqfs
+sleep 1
+mount /home/gentoo-current.xz.sqfs /usr/portage
+sleep 1
+/bin/cp -a /var/cache/eix/portage.eix /var/cache/eix/previous.eix
+sleep 1
+/usr/bin/eix-update
+sleep 1
+/usr/bin/eix-diff
