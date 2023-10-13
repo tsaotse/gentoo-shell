1da474e507f5f39a8a25340661a864ec275fe7cc
tsaotse <78033148+tsaotse@users.noreply.github.com>
2023-10-14 07:56:55 +0800

    Create overlay-elementary-sqfs.sh

---
+++ overlay-elementary-sqfs.sh
@@ -0,0 +1,14 @@
+### local.overlay.elementary ###
+umount -l /usr/local/overlay/elementary
+sleep 1
+mv -v /home/elementary.zx.sqfs /home/old.elementary.zx.sqfs
+sleep 1
+rm -rfv /tmp/elementary*
+wget -c -O /tmp/elementary.zip https://ghproxy.com/https://github.com/pimvullers/elementary/archive/refs/heads/master.zip
+sleep 1
+unzip /tmp/elementary.zip -d /tmp
+sleep 1
+mksquashfs /tmp/elementary-master/ /home/elementary.zx.sqfs -comp xz
+sleep 1
+mount /home/elementary.zx.sqfs /usr/local/overlay/elementary
+###
