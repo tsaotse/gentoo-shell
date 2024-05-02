a6a6dd7dee61c712c9a43cccb6234a70a131ad66
tsaotse <78033148+tsaotse@users.noreply.github.com>
2024-05-03 07:31:58 +0800

    Update portage.sqfs.sh
    add --no-check-certificate for wget

--- portage.sqfs.sh
+++ portage.sqfs.sh
@@ -2,7 +2,7 @@ umount -l /usr/portage
 sleep 1
 mv -v /home/gentoo-current.xz.sqfs /home/old.gentoo-current.xz.sqfs
 sleep 1
-wget -c -O /home/gentoo-current.xz.sqfs https://mirrors.tuna.tsinghua.edu.cn/gentoo/snapshots/squashfs/gentoo-current.xz.sqfs
+wget -c -O /home/gentoo-current.xz.sqfs --no-check-certificate https://mirrors.tuna.tsinghua.edu.cn/gentoo/snapshots/squashfs/gentoo-current.xz.sqfs
 sleep 1
 mount /home/gentoo-current.xz.sqfs /usr/portage
 sleep 1
